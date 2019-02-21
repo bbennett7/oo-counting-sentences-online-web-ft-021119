80d3d302c5074ba713eb468fb0d0ef8723eb6203
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-21 06:36:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,10 +11,11 @@ class String
   end
 
   def question?
-  if self.end_with?("?")
-    true 
-  else 
-    false 
+    if self.end_with?("?")
+      true 
+    else 
+      false 
+    end
   end
 
   def exclamation?
